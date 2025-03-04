3486d645cf6e81b557fade3fa7f49f6129062234
Jose Vasconcelos <tadeo199928j@gmail.com>
2025-03-04 08:37:21 +0000

    Button function

--- buscaminas/src/App.js
+++ buscaminas/src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Celda from './Celda'
 import Marcador from './Marcado';
 
 function App() {
   //Def val/ props
-  const mapaValores=["1","1","1","0","0","1","*","1","0","0","1","1","2","2","1","0","1","*","*","1","0","1","2","2","1"];
+  const [mapaValores, setMapaValores]=useState(Array(25).fill(""));
   const celdas=mapaValores.map((item,index)=>
     <div className="col-auto p-0" key={index}><Celda valor={item}/></div> 
   );
+
+  // function answer button
+  const btnComenzar= ()=>{
+    setMapaValores(["1","1","1","0","0","1","*","1","0","0","1","1","2","2","1","0","1","*","*","1","0","1","2","2","1"]);
+  }
+
+  const btnRestart = ()=>{
+    setMapaValores(Array(25).fill(" ")); 
+  }
+
   return (
     <div className="container text-center" style={{ width: 340 }}>
       <div className="grid bg-body-secondary py-2 px-4 borderOutSide m-0">
@@ -16,7 +26,7 @@ function App() {
           <div className="d-flex flex-wrap justify-content-around">
             <Marcador Minas="100"/>
             <div className="align-self-center m-2 borderInsideS">
-              <img src="acierto.png" style={{ width: 50 }} />
+              <img src="acierto.png" style={{ width: 50 }} alt="Smile"/>
             </div>
             <div
               className="lcdText text-danger pe-2 m-2 borderInsideS"
@@ -34,6 +44,9 @@ function App() {
           </div>
         </div>
       </div>
+      <div><button className="btn btn-outline-secondary mt-2" onClick={btnComenzar}>Comienza la Partida</button></div>
+      <div><button className="btn btn-outline-secondary mt-2" onClick={btnRestart}>Reinicio</button></div>
+  
     </div>
   );
 }
